361ccfde5b85d1c243cbbf80099d681e7ec7bf89
Rodriguez Serrano <128393792+prof-jose@users.noreply.github.com>
2023-12-19 17:09:37 +0100

    doc: docstrings & flake8

--- protolearn/model.py
+++ protolearn/model.py
@@ -17,6 +17,25 @@ class PrototypeLayer(tf.keras.layers.Layer):
             self, units=3, scale=1.0, eta=1e-4, means=None,
             trainable_scales=False
             ):
+        """
+        Define a probabilistic layer that computes the log-likelihood of the
+        input data based on a multivariate normal distribution with
+        specified means and scales.
+
+        Parameters
+        ----------
+        units : int
+            Number of prototypes to use.
+        scale : float
+            Scale of the multivariate normal distribution.
+        eta : float
+            Regularization constant.
+        means : array-like
+            Initial means for the multivariate normal distribution.
+        trainable_scales : bool
+            Whether to train the scales.
+            If True, the value given in scale is the initializer.
+        """
         super().__init__()
         self._units = units
         self._eta = eta
@@ -25,6 +44,14 @@ class PrototypeLayer(tf.keras.layers.Layer):
         self._trainable_scales = trainable_scales
 
     def build(self, input_shape):
+        """
+        Specify the building process of this layer.
+
+        Parameters
+        ----------
+        input_shape : tuple
+            Shape of the input data.
+        """
         self._means = self.add_weight(
                 shape=(self._units, input_shape[-1]),
                 initializer="random_normal",
@@ -47,6 +74,19 @@ class PrototypeLayer(tf.keras.layers.Layer):
         )
 
     def call(self, X):
+        """
+        Call the layer: compute the log-likelihood of the input data.
+
+        Parameters
+        ----------
+        X : array-like
+            Input data.
+
+        Returns
+        -------
+        loglike : array-like
+            Log-likelihood of the input data.
+        """
 
         distances = tf.norm(X[:, None, :] - self._means, axis=-1)
         avg_min_distance = tf.reduce_mean(tf.reduce_min(distances, axis=0))
@@ -56,30 +96,42 @@ class PrototypeLayer(tf.keras.layers.Layer):
 
 
 def get_rlvq_model(dim=2, n_prototypes=3, scale=0.1, reg_constant=0.001,
-                   means=None, values=None, activation="linear", trainable_scales=False,
-                   projection=None):
+                   means=None, values=None, activation="linear",
+                   trainable_scales=False):
+    """
+    Get a RLVQ model with specified parameters.
+
+    Parameters
+    ----------
+    dim : int
+        Dimensionality of the input data.
+    n_prototypes : int
+        Number of prototypes to use.
+    scale : float
+        Scale of the multivariate normal distribution.
+    reg_constant : float
+        Regularization constant.
+    means : array-like
+        Initial means for the multivariate normal distribution.
+    values : array-like
+        Initial values for the prototypes.
+    activation : str
+        Activation function for the output layer.
+    trainable_scales : bool
+        Whether to train the scales.
+        If True, the value given in scale is the initializer.
+
+    Returns
+    -------
+    model : tf.keras.models.Model
+        Prototype-based model, ready for training
+    """
 
     input = tf.keras.layers.Input(shape=dim)
 
-    # projection is an array specifying the number of units in each layer
-
-#    if projection is not None:
-#        h = tf.keras.layers.Dense(
-#            projection[0], activation="relu"
-#            )(input)
-#        for i in range(1, len(projection)):
-#            h = tf.keras.layers.Dense(
-#                projection[i], activation="relu"
-#                )(h)
-#        loglike = PrototypeLayer(units=n_prototypes, scale=scale, eta=reg_constant,
-#                             means=means, trainable_scales=trainable_scales)(h)
-
-#    else:
-#        loglike = PrototypeLayer(units=n_prototypes, scale=scale, eta=reg_constant,
-#                             means=means, trainable_scales=trainable_scales)(input)
-
-    loglike = PrototypeLayer(units=n_prototypes, scale=scale, eta=reg_constant,
-                             means=means, trainable_scales=trainable_scales)(input)
+    loglike = PrototypeLayer(units=n_prototypes, scale=scale,
+                             eta=reg_constant, means=means,
+                             trainable_scales=trainable_scales)(input)
     probs = tf.keras.layers.Softmax(name="softmax")(-loglike)
     if values is None:
         regression = tf.keras.layers.Dense(
@@ -97,7 +149,28 @@ def get_rlvq_model(dim=2, n_prototypes=3, scale=0.1, reg_constant=0.001,
 
 
 def init_means_and_values(X, y, n_prototypes, method="kmeans"):
-    # Initialize the means and values
+    """
+    Initialize the means and values for the prototypes.
+
+    Parameters
+    ----------
+    X : array-like
+        Input data.
+    y : array-like
+        Target values.
+    n_prototypes : int
+        Number of prototypes to use.
+    method : str
+        Method to use for initialization.
+        Options: "kmeans", "random_pick"
+
+    Returns
+    -------
+    means : array-like
+        Initial means for the multivariate normal distribution.
+    values : array-like
+        Initial values for the prototypes.
+    """
 
     if method == "kmeans":
         kmeans = KMeans(n_clusters=n_prototypes, n_init="auto")
